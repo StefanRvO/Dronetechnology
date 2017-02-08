416eb2c9059617decd0117a42a303aa1b282e025
Stefan Ravn van Overeem <stefan@stefanrvo.dk>
2017-02-08 19:19:38 +0100

    Added stuff for plotting nmea data

--- Lecture1/exercise_nmea_data/main.py
+++ Lecture1/exercise_nmea_data/main.py
@@ -11,10 +11,8 @@ kml.trksegbegin("drone", "route", "yellowLineGreenPoly", "absolute")
 # Open file for reading
 with open("nmea_trimble_gnss_eduquad_flight.txt", "r") as data_file:
     for line in data_file:
-        current_line = line.split(',')  # Split the line by commas into a array
-
         # Only read lines if they are not empty
-        if current_line[0] == "$GPGGA":
+        if "$GPGGA" in line:
             msg = pynmea2.parse(line)
 
             kml.trkpt(float(msg.latitude), float(msg.longitude), float(msg.altitude))
