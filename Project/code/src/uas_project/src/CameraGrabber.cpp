85dd513b68ea2f113c508184df484797280fec6b
tobiasrj20 <tobiasrj20@gmail.com>
2017-05-08 17:12:45 +0200

    Camera node is working.

--- Project/code/src/uas_project/src/CameraGrabber.cpp
+++ Project/code/src/uas_project/src/CameraGrabber.cpp
@@ -1,57 +1,38 @@
 #include "ros/ros.h"
 #include "opencv2/opencv.hpp"
 #include "std_msgs/String.h"
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/image_encodings.h>
 #include <cv_bridge/cv_bridge.h>
 
 using namespace std;
 using namespace cv;
+
 /*
-
-Mat cameraFrame;
-VideoCapture camera(0); // Open default camera 0
-
-if (!camera.isOpened())
+*   Convert openCV image into ROS image message
+*/
+sensor_msgs::Image imageToMsg(Mat &img)
 {
-    cout << "ERROR: Cannot open camera!" << endl;
-    return;
+    cv_bridge::CvImage img_bridge;
+    sensor_msgs::Image img_msg;     // Message to be sent
+    std_msgs::Header header; // empty header
+
+    header.seq = 1; // user defined counter
+    header.stamp = ros::Time::now(); // time
+
+    img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, img);
+    img_bridge.toImageMsg(img_msg); // from cv_bridge to sensor_msgs::Image
+
+    return img_msg;
 }
 
-while (1)
-{
-    camera.read(cameraFrame);   // Grab image from webcam
 
-    imshow("Webcam", cameraFrame);
-
-    if( waitKey(25) == 27 ) // Frame rate: 25ms, stop capturing by pressing ESC.
-        break;
-}*/
 
 int main(int argc, char **argv)
 {
-    /*
-    Mat cameraFrame;
-    VideoCapture camera(0); // Open default camera 0
-
-    if (!camera.isOpened())
-    {
-        cout << "ERROR: Cannot open camera!" << endl;
-        return 0;
-    }
-
-    while (1)
-    {
-        camera.read(cameraFrame);   // Grab image from webcam
-
-        imshow("Webcam", cameraFrame);
-
-        if( waitKey(25) == 27 ) // Frame rate: 25ms, stop capturing by pressing ESC.
-            break;
-    }*/
-
-
-    ros::init(argc, argv, "image_grabber");
-    ros::NodeHandle nh;
-    ros::Publisher pub = nh.advertise<std_msgs::String>("camera_image", 1000);
+    ros::init(argc, argv, "image_grabber"); // Init ROS
+    ros::NodeHandle nh; // Node handler
+    ros::Publisher pub = nh.advertise<sensor_msgs::Image>("camera_image", 5);   // Image publisher
 
     Mat cameraFrame;
     VideoCapture camera(0); // Open default camera 0
@@ -66,12 +47,8 @@ int main(int argc, char **argv)
     {
         camera.read(cameraFrame);   // Grab image from webcam
 
-        //sensor_msgs::ImagePtr toImageMsg() const;
-        //void toImageMsg(sensor_msgs::Image& ros_image) const;
-        sensor_msgs::ImagePtr img = toImageMsg(cameraFrame);
-
-        std_msgs::String msg;
-        pub.publish(msg);
+        sensor_msgs::Image img_msg = imageToMsg(cameraFrame);   // Convert webcam image to a ROS message
+        pub.publish(img_msg); // Publish image on ROS
         ros::spinOnce();
     }
 
